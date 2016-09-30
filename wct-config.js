94533a8219e597b721a8c96950785980a7811ac0
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2016-09-30 10:28:46 -0400

    Add wct-config.js

---
+++ wct-config.js
@@ -0,0 +1,20 @@
+var baseConfig = require('./polymer-config'),
+	extend = require('extend');
+
+var wctConfig = {
+  "env": {
+    "es6": true,
+    "mocha": true
+  },
+  "globals": {
+    "a11ySuite": true,
+    "fixture": true,
+    "flush": true,
+    "expect": true,
+    "MockInteractions": true,
+    "sinon": true,
+    "WCT": true
+  }
+};
+
+module.exports = extend(true, polymerConfig, wctConfig);
