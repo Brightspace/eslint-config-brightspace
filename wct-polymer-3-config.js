4c9a998ab9cf94b548056c4ccd9ff4625a2ebb48
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2018-10-19 11:14:23 -0400

    Add polymer 3 configs and update readme

---
+++ wct-polymer-3-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": "./polymer-3-config.js",
+  "env": {
+    "es6": true,
+    "mocha": true
+  },
+  "globals": {
+    "a11ySuite": true,
+    "assert": true,
+    "fixture": true,
+    "flush": true,
+    "expect": true,
+    "MockInteractions": true,
+    "sinon": true,
+    "WCT": true
+  }
+};
