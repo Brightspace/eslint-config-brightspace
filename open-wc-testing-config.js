08b826284a8f532ca3bc6db3b09683ece3a5e1c7
Dave Lockhart <dave.lockhart@d2l.com>
2020-02-03 15:17:40 -0500

    Fix open wc testing config (#30)
    * adding node and no-invalid-this for visual-diff tests
    
    * increasing version

--- open-wc-testing-config.js
+++ open-wc-testing-config.js
@@ -1,6 +1,10 @@
 module.exports = {
-	"extends": "./lit-config.js",
-	"env": {
-		"mocha": true
-	}
+  "extends": "./lit-config.js",
+    "env": {
+      "mocha": true,
+      "node": true
+  },
+  "rules": {
+    "no-invalid-this": 0
+  }
 };
