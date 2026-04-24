bbb5a478e1f8d07291ebcd8a1845dc1934459b95
Dave Lockhart <dlockhart@users.noreply.github.com>
2026-04-24 13:03:13 -0400

    GAUD-9816: add mocha plugin

--- configs/testing.js
+++ configs/testing.js
@@ -1,16 +1,22 @@
-import globals from 'globals';
 import litConfig from './lit.js';
+import mochaPlugin from 'eslint-plugin-mocha';
 
 export default [
 	...litConfig,
+	mochaPlugin.configs.recommended,
 	{
-		'languageOptions': {
-			'globals': {
-				...globals.mocha,
-			}
+		'plugins': {
+			mochaPlugin
 		},
 		'rules': {
-			'no-invalid-this': 0
+			'mocha/consistent-spacing-between-blocks': 'off',
+			'mocha/max-top-level-suites': 'off',
+			'mocha/no-identical-title': 'warn',
+			'mocha/no-exclusive-tests': 'error',
+			'mocha/no-mocha-arrows': 'off',
+			'mocha/no-return-from-async': 'error',
+			'mocha/no-setup-in-describe': 'off',
+			'no-invalid-this': 0,
 		}
 	}
 ];
