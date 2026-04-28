1456e46129a2d0810d492176695f4fd0b2f5851d
Devon Powell <Devon.Powell@D2L.com>
2026-04-28 10:04:44 -0400

    Sort rules

--- configs/base.js
+++ configs/base.js
@@ -7,12 +7,7 @@ export default [js.configs.recommended, {
 		'@stylistic': stylistic
 	},
 	'rules': {
-		'@stylistic/comma-spacing': 2, // nr
-		'@stylistic/eol-last': 2, // nr
 		'eqeqeq': [2, 'always', { 'null': 'ignore' }], // nr
-		'@stylistic/keyword-spacing': 2,
-		'@stylistic/linebreak-style': ['error', 'unix'], // nr
-		'@stylistic/new-parens': 2, // nr
 		'no-debugger': 2,
 		'no-dupe-args': 2,
 		'no-dupe-keys': 2,
@@ -20,10 +15,7 @@ export default [js.configs.recommended, {
 		'no-ex-assign': 2,
 		'no-fallthrough': 2,
 		'no-invalid-this': 2, // nr
-		'@stylistic/no-multiple-empty-lines': [2, { 'max': 1 }], // nr
-		'@stylistic/no-multi-spaces': 2, // nr
 		'no-new-wrappers': 2, // nr
-		'@stylistic/no-trailing-spaces': 2, // nr
 		'no-undef': 2,
 		'no-unneeded-ternary': 2, // nr
 		'no-unreachable': 2,
@@ -35,16 +27,24 @@ export default [js.configs.recommended, {
 		}],
 		'no-use-before-define': [2, 'nofunc'], // nr
 		'no-var': 2, // nr
-		'@stylistic/object-curly-spacing': [2, 'always'],
 		'prefer-const': 2, // nr
+		'strict': [2, 'global'], // nr
+		'valid-typeof': 2,
+		'@stylistic/comma-spacing': 2, // nr
+		'@stylistic/eol-last': 2, // nr
+		'@stylistic/indent': [2, 'tab', { 'SwitchCase': 1 }], // nr
+		'@stylistic/keyword-spacing': 2,
+		'@stylistic/linebreak-style': ['error', 'unix'], // nr
+		'@stylistic/new-parens': 2, // nr
+		'@stylistic/no-multi-spaces': 2, // nr
+		'@stylistic/no-multiple-empty-lines': [2, { 'max': 1 }], // nr
+		'@stylistic/no-trailing-spaces': 2, // nr
+		'@stylistic/object-curly-spacing': [2, 'always'],
+		'@stylistic/quotes': [2, 'single', { 'avoidEscape': true }], // nr
 		'@stylistic/semi': 2, // nr
 		'@stylistic/space-before-blocks': [2, 'always'], // nr
 		'@stylistic/space-before-function-paren': [2, 'never'], //nr
 		'@stylistic/space-in-parens': [2, 'never'],
-		'@stylistic/space-infix-ops': 2, // nr
-		'strict': [2, 'global'], // nr
-		'valid-typeof': 2,
-		'@stylistic/indent': [2, 'tab', { 'SwitchCase': 1 }], // nr
-		'@stylistic/quotes': [2, 'single', { 'avoidEscape': true }] // nr
+		'@stylistic/space-infix-ops': 2 // nr
 	}
 }];
